8f5d088548e81adafa2ad4c7adc88fa59e6bbec8
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-05-15 23:48:03 +0530

    Auth Demo

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,15 +1,88 @@
-var express = require("express");
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    LocalStrategy         = require("passport-local"),
+    User                  = require("./models/user"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "123456789",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// ==============
+// ROUTES
+// ==============
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 })
+
+// Auth Routes
+
+// show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// handling user sign up
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.render("register");
+        }
+        else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            });
+        }
+    });
+})
+
+// LOGIN ROUTES
+// render login form
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+// handling user login
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req,res){
+});
+
+// LOGOUT ROUTES
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function(){
     console.log("Server Started");
--- Authentication/readme.md
+++ Authentication/readme.md
@@ -14,3 +14,19 @@
 * Install needed packages
 * Add root route and template
 * Add secret route and template
+
+# Auth Code Along Part 2
+* Create User Model
+* Configure Passport
+
+# Auth Code Along Part 3
+* Add Register routes
+* Add Register form
+
+# Auth Code Along Part 4
+* Add Login Routes
+* Add Login Form
+
+# Auth Code Along Part 5
+* Add Logout Route
+* Add isLoggedIn middleware
