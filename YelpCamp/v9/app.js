ec6761b633f881edbd2fbea5af971ffa504b1652
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-05-19 10:21:10 +0530

    v9

--- YelpCamp/v9/app.js
+++ YelpCamp/v9/app.js
@@ -2,7 +2,6 @@ var express        = require("express"),
     app            = express(),
     bodyParser     = require("body-parser"),
     mongoose       = require("mongoose"),
-    flash          = require("connect-flash"),
     passport       = require("passport"),
     LocalStrategy  = require("passport-local"),
     methodOverride = require("method-override"),
@@ -21,7 +20,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-app.use(flash());
 // seedDb(); // seed the database     
 
 // PASSPORT CONFIGURATION
