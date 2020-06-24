a9000389693fc225aec74762b71a45dba475121b
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-06-24 17:20:33 +0530

    Class Components

--- ReactJS/hello-world/src/App.js
+++ ReactJS/hello-world/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
+import Greet from './components/Greet';
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
