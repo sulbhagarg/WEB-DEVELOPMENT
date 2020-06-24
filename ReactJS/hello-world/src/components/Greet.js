98320c0456552a3dcff956f01a051572fd46f65e
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-06-24 17:11:28 +0530

    functional components

---
+++ ReactJS/hello-world/src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Hello Sulbha</h1>
+// }
+
+// ES6 syntax
+const Greet = () => <h1>Hello Sulbha</h1>
+
+// importing it as name Greet is compulsion!
+// export const Greet = () => <h1>Hello Sulbha</h1>
+
+// This allows to import from anyname!!
+export default Greet
