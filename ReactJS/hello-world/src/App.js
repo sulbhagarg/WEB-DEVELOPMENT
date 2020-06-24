e7287f76a97a4d77e83420b2b15992179b2c5b0a
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-06-24 18:37:36 +0530

    Click Event Handlers

--- ReactJS/hello-world/src/App.js
+++ ReactJS/hello-world/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <FunctionClick />
+      <ClassClick />
+      {/* <Counter /> */}
       {/* <Message /> */}
       {/* <Greet name="Ross" love="Dinasorus/Rachel">
         <p>
